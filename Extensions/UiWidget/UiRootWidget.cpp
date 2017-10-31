c95d8bd6c2935acf4646b5dffe37c8066f6c6ae6
TrevorSundberg <Trevor.Sundberg@gmail.com>
2017-10-31 13:49:06 -0700

    Missed lines in merge.

--- Extensions/UiWidget/UiRootWidget.cpp
+++ Extensions/UiWidget/UiRootWidget.cpp
@@ -131,6 +131,8 @@ void UiRootWidget::Initialize(CogInitializer& initializer)
     // Keyboard events
     ConnectThisTo(Keyboard::Instance, Events::KeyDown, OnKeyboardEvent);
     ConnectThisTo(Keyboard::Instance, Events::KeyRepeated, OnKeyboardEvent);
+    ConnectThisTo(Keyboard::Instance, Events::KeyUp, OnKeyboardEvent);
+    ConnectThisTo(Keyboard::Instance, Events::TextTyped, OnKeyboardEvent);
   }
 }
 
