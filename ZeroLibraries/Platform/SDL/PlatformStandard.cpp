2f6205897a955e101d91d968812711d5dce840ca
trevor.sundberg <trevor.sundberg@zeroengine.io>
2018-06-01 09:38:35 -0700

    Added SDL initialization and changed a void* to OsHandle.

--- ZeroLibraries/Platform/SDL/PlatformStandard.cpp
+++ ZeroLibraries/Platform/SDL/PlatformStandard.cpp
@@ -17,6 +17,8 @@ SDL_Cursor* gSDLCursors[SDL_NUM_SYSTEM_CURSORS] = { 0 };
 //**************************************************************************************************
 void PlatformLibrary::Initialize()
 {
+  SDL_Init(SDL_INIT_EVERYTHING);
+
   for (size_t i = 0; i < SDL_NUM_SYSTEM_CURSORS; ++i)
     gSDLCursors[i] = SDL_CreateSystemCursor((SDL_SystemCursor)i);
 }
@@ -26,6 +28,8 @@ void PlatformLibrary::Shutdown()
 {
   for (size_t i = 0; i < SDL_NUM_SYSTEM_CURSORS; ++i)
     SDL_FreeCursor(gSDLCursors[i]);
+
+  SDL_Quit();
 }
 
 }//namespace Zero
