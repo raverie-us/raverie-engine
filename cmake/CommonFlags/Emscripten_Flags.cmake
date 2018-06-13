3a02d486bd4018862d85530fea5fb6e86ce98e73
dane.curbow <dane.curbow@zeroengine.io>
2018-06-12 17:44:31 -0700

    Fixed need to remove quotes from generated make files around the compiler flags.

--- cmake/CommonFlags/Emscripten_Flags.cmake
+++ cmake/CommonFlags/Emscripten_Flags.cmake
@@ -4,9 +4,9 @@
 # configured for setting the emcc flags
 ################################################################################
 
-set(common_flags
-    "-DEMSCRIPTEN=1 -D__EMSCRIPTEN__=1 -s WASM=1 -s USE_SDL=2 -s ALLOW_MEMORY_GROWTH=1 -s USE_WEBGL2=1 -s FULL_ES2=1 -s FULL_ES3=1 -msse -msse2 -std=c++11 -fstack-protector -O0 -fdelayed-template-parsing -fexceptions -fno-omit-frame-pointer -frtti -DZeroExportDll"
-)
+# When using our defined common_flags variable all -s flags following the first one are removed and results in an invalid command line argument.
+# By directly setting CMAKE_CXX_FLAGS with our compiler flags the subsequent -s flags remain and the project compiles properly.
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DEMSCRIPTEN=1 -D__EMSCRIPTEN__=1 -s WASM=1 -s USE_SDL=2 -s ALLOW_MEMORY_GROWTH=1 -s USE_WEBGL2=1 -s FULL_ES2=1 -s FULL_ES3=1 -msse -msse2 -std=c++11 -fstack-protector -O0 -fdelayed-template-parsing -fexceptions -fno-omit-frame-pointer -frtti -DZeroExportDll" )
 
 set(common_linker_flags
     "-DEMSCRIPTEN=1 -D__EMSCRIPTEN__=1 -s ALLOW_MEMORY_GROWTH=1 -s WASM=1 -s USE_SDL=2 -s FORCE_FILESYSTEM=1 -s USE_WEBGL2=1 -s FULL_ES2=1 -s FULL_ES3=1 -msse -msse2 -std=c++11"
