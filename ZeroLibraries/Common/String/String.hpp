c7b2d7d07f40e5c6372e428b67e62f66e63a229c
joshua.davis <joshua.davis@zeroengine.io>
2018-06-15 10:13:49 -0700

    Fixed a memory leak in StringJoin. Reviewed by trevor.sundberg. -

--- ZeroLibraries/Common/String/String.hpp
+++ ZeroLibraries/Common/String/String.hpp
@@ -210,7 +210,15 @@ public:
       ++i;
     }
 
-    return JoinInternal(separator, values, count);
+    String result = JoinInternal(separator, values, count);
+
+    // Have to manually call the destructor on every string range since we called placement new
+    for(size_t i = 0; i < count; ++i)
+    {
+      values[i].~StringRange();
+    }
+
+    return result;
   }
 
   template <typename RangeType>
