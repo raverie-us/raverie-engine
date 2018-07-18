1da909bf7ff6ede0501e252a2f2260b08129a9eb
Joshua Shlemmer <joshuashlem@gmail.com>
2018-07-18 16:26:35 -0700

    Added cleaning of BuildOut folders and renamed Emscripten output folder.

--- CMakeBuild/GenerateVS2015WithUnitTestsDebugging.cmd
+++ CMakeBuild/GenerateVS2015WithUnitTestsDebugging.cmd
@@ -5,8 +5,13 @@ rd %folder_name% /s/q
 
 mkdir %folder_name%
 
-cd %folder_name%
+pushd %folder_name%
 
-cmake -G "Visual Studio 14 2015" -Dgenerate_with_unit_tests=ON -DWindows_VS_2015=ON -DBits_32=ON --warn-uninitialized ../..
+set config=Windows_VS_2015
 
+cmake -E remove_directory ..\..\BuildOutput\Out\%config%
+
+cmake -G "Visual Studio 14 2015" -Dgenerate_with_unit_tests=ON -D%config%=ON -DBits_32=ON --warn-uninitialized ../..
+
+popd
 popd
--- cmake/Configuration/Windows_Emscripten.cmake
+++ cmake/Configuration/Windows_Emscripten.cmake
@@ -7,4 +7,4 @@ include(${cmake_os_dir}/Emscripten.cmake)
 include(${cmake_compiler_dir}/Clang.cmake)
 include(${cmake_flags_dir}/Emscripten_Flags.cmake)
 
-set(configuration Windows_Emscripten)
+set(configuration Emscripten)
