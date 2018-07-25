dacb587a1388693021c4c4f995f8a87affb4b745
nathan.carlson <nathan.carlson@zeroengine.io>
2018-07-25 14:52:59 -0700

    Replaced temporary MaterialBlock meta creation with desired implementation.-
    Summary:
    Fixed MaterialBlock getters for script.-
    
    Marking Material inputs as modified when MaterialBlock properties are changed.-
    
    Test Plan: In a renderer, change a Material input before a render pass that uses it and then change it again and do the same render pass again. Something like a vertex position offset on a fragment should be easy to verify that unique property values were captured for each render pass.
    
    Reviewers: trevor.sundberg, joshua.claeys, joshua.davis
    
    Reviewed By: trevor.sundberg
    
    Subscribers: nathan.carlson
    
    Tags: PHID-PROJ-g5eliiiwbwbw5dr6st5w
    
    Differential Revision: https://dev.zeroengine.io/D644

--- Systems/Graphics/MaterialBlock.hpp
+++ Systems/Graphics/MaterialBlock.hpp
@@ -7,7 +7,7 @@ namespace Zero
 {
 
 /// Represents meta data of shader fragment types that can be composited together on Materials.
-class MaterialBlock
+class MaterialBlock : public IZilchObject
 {
 public:
   ZilchDeclareInheritableType(TypeCopyMode::ReferenceType);
@@ -19,9 +19,24 @@ public:
 
   Material* mOwner;
 
+  void MarkAsModified();
   IndexRange AddShaderInputs(Array<ShaderInput>& shaderInputs);
 };
 
 typedef HandleOf<MaterialBlock> MaterialBlockHandle;
+
+// Meta functions for shader fragments that inherit from MaterialBlock.
+
+// Constructor/Destructor for fragments being created in a ZilchScript.
+void FragmentConstructor(Call& call, ExceptionReport& report);
+void FragmentDestructor(Call& call, ExceptionReport& report);
+
+// Getter/Setter for primitive types that are directly memory copyable.
+void FragmentGetter(Call& call, ExceptionReport& report);
+void FragmentSetter(Call& call, ExceptionReport& report);
+
+// Getter/Setter for Texture handles.
+void FragmentTextureGetter(Call& call, ExceptionReport& report);
+void FragmentTextureSetter(Call& call, ExceptionReport& report);
 
 } // namespace Zero
