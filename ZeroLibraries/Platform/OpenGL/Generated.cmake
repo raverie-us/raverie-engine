3bb17616754ce3ed454b7485c83523fb183d18e5
TrevorSundberg <Trevor.Sundberg@gmail.com>
2018-11-15 01:39:52 -0800

    Getting rid of Generated cmake files.

--- ZeroLibraries/Platform/OpenGL/Generated.cmake
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Generated using Joshua T. Fisher's 'CMake Builder'.
-# Link: https://github.com/playmer/CmakeBuilder 
-################################################################################
-target_sources(OpenGL
-  PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/OpenglRenderer.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/OpenglRenderer.hpp
-)
--- ZeroLibraries/Platform/SDLSTLEmpty/Generated.cmake
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Generated using Joshua T. Fisher's 'CMake Builder'.
-# Link: https://github.com/playmer/CmakeBuilder 
-################################################################################
-target_sources(Platform
-  PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/Precompiled.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/Precompiled.hpp
-)
