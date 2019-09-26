a055942b6a66c91eb1a344ecd01c4e4a292c263a
Trevor Sundberg <tsundberg@s2.systems>
2019-09-25 19:03:24 -0700

    Move where we cache docker image

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -e
 docker load -i ./Build/Cache/welder.tar || true
 ./dockerbuild.sh
+docker save -o ./Build/Cache/welder.tar welder
 ./weld.sh node index.js cmake --alias=$ALIAS
 ./weld.sh node index.js build --alias=$ALIAS
-docker save -o ./Build/Cache/welder.tar welder
