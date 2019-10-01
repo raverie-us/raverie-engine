6db8783120fa1d2d91131d3c3512e394114efd90
Trevor Sundberg <tsundberg@s2.systems>
2019-10-01 09:03:58 -0700

    Merge the builds cript into Travis

--- build.sh
+++
@@ -1,6 +0,0 @@
-set -e
-docker load -i ./Build/Cache/welder.tar || true
-./dockerbuild.sh
-docker save -o ./Build/Cache/welder.tar welder
-./weld.sh node index.js cmake "$@"
-./weld.sh node index.js build "$@"
