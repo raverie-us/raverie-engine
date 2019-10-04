deca12f603b6d42c05ef1731b4a12af98d6e19cd
Trevor Sundberg <tsundberg@s2.systems>
2019-10-04 08:46:42 -0700

    Attempt to build again on Travis

--- weld.sh
+++ weld.sh
@@ -1,3 +1,3 @@
 set -e
 mkdir -p Build/Cache
-docker run --rm --user `id -u`:`id -g` -v"`pwd`/Build/Cache":'/cache/' -v`pwd`:`pwd` -w`pwd` welder "$@"
+docker run --memory=8g --memory-swap=32g --oom-kill-disable --rm --user `id -u`:`id -g` -v"`pwd`/Build/Cache":'/cache/' -v`pwd`:`pwd` -w`pwd` welder "$@"
