9309a608a3894bffd967dfe83c9e4997a80a3302
Robert Campbell <waprave@gmail.com>
2022-02-15 12:52:28 -0500

    Moved CodeTranslator to Editor. It is only used directly in Editor. Avoid linking directly to CodeTranslator in Launcher.

--- Code/Extensions/CodeTranslator/CodeTranslatorStandard.hpp
+++
@@ -1,9 +0,0 @@
-// MIT Licensed (see LICENSE.md).
-#pragma once
-
-#include "Editor/Editor/EditorStandard.hpp"
-#include "Extensions/ZilchShaders/ZilchShadersStandard.hpp"
-
-#include "CodeSplitWindow.hpp"
-#include "CodeTranslatorCommands.hpp"
-#include "ShaderTranslationDebugHelper.hpp"
