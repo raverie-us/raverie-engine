4898bd912566fd6aeb4d1c5e0ba193b58b8685ab
TrevorSundberg <Trevor.Sundberg@gmail.com>
2022-08-07 18:29:18 -0700

    Add "Working..." text to prebuilt content step

--- index.js
+++ index.js
@@ -930,6 +930,7 @@ const executeBuiltProcess = async (buildDir, combo, libraryDir, library, args) =
 
 const prebuilt = async (options) => {
   console.log("Copying Prebuilt Content");
+  const endPnot = preventNoOutputTimeout();
   const combo = determineCmakeCombo(options);
   rimraf.sync(dirs.prebuiltContent);
 
@@ -955,6 +956,7 @@ const prebuilt = async (options) => {
     printLogLine("Prebuilt content directory did not exist or was empty");
   }
   console.log("Copied Prebuilt Content");
+  endPnot();
 };
 
 const pack = async (options) => {
