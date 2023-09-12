ed90d3e4518ea5007b83ba7d7bf5f8fe005cb6ea
TrevorSundberg <Trevor.Sundberg@gmail.com>
2023-09-12 12:16:19 -0700

    Remove non-needed plugin to vite (no longer need Buffer/WASI from npm)

--- Browser/vite.config.ts
+++ Browser/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import nodePolyfills from "vite-plugin-node-stdlib-browser";
 
 export default defineConfig({
   server: {
     port: 8080,
-  },
-  plugins: [nodePolyfills()]
+  }
 });
