1b90a41122bfc6cf1ab8627927bc6970c24b8686
TrevorSundberg <Trevor.Sundberg@gmail.com>
2023-09-14 12:53:24 -0700

    Remove FileSystem retry since we're using the VFS

--- Code/Foundation/Common/Platform/FileSystem.cpp
+++ Code/Foundation/Common/Platform/FileSystem.cpp
@@ -29,34 +29,10 @@ String GetApplicationDirectory()
   return FilePath::GetDirectoryPath(GetApplication());
 }
 
-template <typename Function>
-bool TryFileOperation(StringParam dest, StringParam source, Function fileOp)
-{
-  bool result = false;
-  // Keep trying to copy the file until we succeed up to some max number of
-  // times. This is attempting to deal with random file locks (from anti-virus
-  // or something).
-  for (size_t i = 0; i < 10; ++i)
-  {
-    result = fileOp(dest, source);
-    if (result == true)
-      break;
-
-    Os::Sleep(1);
-  }
-
-  // Maybe log some extra error messages here?
-  if (result == false)
-  {
-  }
-
-  return result;
-}
-
 bool CopyFile(StringParam dest, StringParam source)
 {
   FileModifiedState::BeginFileModified(dest);
-  bool result = TryFileOperation(dest, source, CopyFileInternal);
+  bool result = CopyFileInternal(dest, source);
   FileModifiedState::EndFileModified(dest);
   return result;
 }
@@ -64,7 +40,7 @@ bool CopyFile(StringParam dest, StringParam source)
 bool MoveFile(StringParam dest, StringParam source)
 {
   FileModifiedState::BeginFileModified(dest);
-  bool result = TryFileOperation(dest, source, MoveFileInternal);
+  bool result = MoveFileInternal(dest, source);
   FileModifiedState::EndFileModified(dest);
   return result;
 }
