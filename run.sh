a5c0725299a0c73ae9c09890f8428c313ca08213
TrevorSundberg <Trevor.Sundberg@gmail.com>
2023-09-28 15:48:48 -0700

    Get run.sh working when in a submodule

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 set -e
 mkdir -p Build/Cache
-docker run --rm --cap-add=SYS_ADMIN --user `id -u`:`id -g` -v"$HOME":"/home/user" -v"`pwd`/Build/Cache":'/cache/' -v`pwd`:`pwd` -w`pwd` raverie-engine "$@"
+
+# We could be running this inside a submodule, and unfortunately submodules when mounted are not
+# recognized as a git repo because their git information is actually contained inside the parent .git file
+# We must mount either this directory or the submodule parent/root directory
+if [[ $(git rev-parse --show-superproject-working-tree) ]]; then
+  DIR=$(git rev-parse --show-superproject-working-tree)
+else
+  DIR=$(pwd)
+fi
+
+docker run --rm --cap-add=SYS_ADMIN --user `id -u`:`id -g` -v"$HOME":"/home/user" -v"`pwd`/Build/Cache":'/cache/' -v$DIR:$DIR -w`pwd` raverie-engine "$@"
